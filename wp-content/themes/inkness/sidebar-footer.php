f10b8ee1ca0461c7ee90c9f34c69d1452531bfdc
lakmal <lakmalwimaladasa@gmail.com>
2016-06-22 12:59:01 +0530

    add new wijet area to footer-slider

--- wp-content/themes/inkness/sidebar-footer.php
+++ wp-content/themes/inkness/sidebar-footer.php
@@ -8,20 +8,25 @@
 	<div id="footer-sidebar" class="widget-area col-md-12" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
-		<div class="footer-column col-md-4"> <?php
+		<div class="footer-column col-md-3"> <?php
 			dynamic_sidebar( 'sidebar-2'); 
 		?> </div> <?php	
 		}
 			
 		if ( is_active_sidebar( 'sidebar-3' ) ) { ?>
-		<div class="footer-column col-md-4"> <?php
+		<div class="footer-column col-md-3"> <?php
 			dynamic_sidebar( 'sidebar-3'); 
 		?> </div> <?php	
 		}
 
 		if ( is_active_sidebar( 'sidebar-4' ) ) { ?>
-		<div class="footer-column col-md-4"> <?php
+		<div class="footer-column col-md-3"> <?php
 			dynamic_sidebar( 'sidebar-4'); 
+		?> </div> <?php	
+		}
+                if ( is_active_sidebar( 'sidebar-5' ) ) { ?>
+		<div class="footer-column col-md-3"> <?php
+			dynamic_sidebar( 'sidebar-5'); 
 		?> </div> <?php	
 		}
 		?>	 	
